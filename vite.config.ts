c4ac6de091a7f1500aee2690adca4110211cd568
partha-794 <pspartha794@gmail.com>
2024-12-25 15:24:26 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/pepperfry-analytics/", // Set base URL for deployment
   server: {
     host: "::",
     port: 8080,
